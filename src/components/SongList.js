7132fc3f27f91d1661ec8432a0e662674777082f
nnastaran83 <nnastaran83@gmail.com>
2022-11-23 14:32:31 +0200

    rendered a list of songs

---
+++ src/components/SongList.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+
+class SongList extends Component {
+    renderList = () => {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <div className="ui button">Select</div>
+                    </div>
+                    <div className="content">
+                        { song.title }
+                    </div>
+                </div>
+            );
+
+
+        });
+    };
+
+    render() {
+        return (
+            <div className="ui middle aligned divided list">
+                { this.renderList() }
+            </div>
+
+        );
+    }
+}
+
+/**
+ * This function will be passed to connect function as the first argument
+   to get some data from the redux store.
+ * The result will be:  this.props = { songs: state.songs}
+                * @param {*} state
+                * @returns A list of songs
+                */
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
